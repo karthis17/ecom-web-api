082d36b8c76665a248166badbd9185f4fb6e3604
karthis17 <karthirs602@gmail.com>
2024-04-16 18:12:53 +0530

    ff

--- index.js
+++ index.js
@@ -20,13 +20,7 @@ app.use(cors({
     origin: ["http://localhost:4200", "http://localhost:3000", "*", "https://ecom-web-lovat.vercel.app"]
 }));
 
-// Enable CORS for all requests
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://ecom-web-lovat.vercel.app');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-});
+
 app.use(cookieParser());
 
 
